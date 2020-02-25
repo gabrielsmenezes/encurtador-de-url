aa4dcb97e718ba2399758374c357a8c609536aa6
Gabriel Menezes <gabrielufmscc@gmail.com>
2020-02-25 13:07:17 -0400

    Retirando o Utils

--- encurtador-de-url-utils/src/main/java/br/com/azinformatica/usuario/utils/config/UtilsConfiguration.java
+++
@@ -1,9 +0,0 @@
-package br.com.azinformatica.usuario.utils.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync
-public class UtilsConfiguration {
-}
